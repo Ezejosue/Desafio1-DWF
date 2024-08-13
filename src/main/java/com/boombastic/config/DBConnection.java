c4047a0780238c67d8bccf49b5c708fab22af274
razmit <rolin.azmitia.iii@gmail.com>
2024-08-12 23:16:01 -0600

    DB Connection

--- src/main/java/com/boombastic/config/DBConnection.java
+++ src/main/java/com/boombastic/config/DBConnection.java
@@ -1,4 +1,20 @@
 package com.boombastic.config;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
+    
+    public Connection getConnection() {
+        Connection con;
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/hr_system", "root", "");
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return con;
+    }
 }
--- src/main/java/com/boombastic/primer/HelloServlet.java
+++ src/main/java/com/boombastic/primer/HelloServlet.java
@@ -2,23 +2,27 @@ package com.boombastic.primer;
 
 import java.io.*;
 
+import com.boombastic.config.DBConnection;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
     private String message;
+    DBConnection db = new DBConnection();
 
     public void init() {
-        message = "Hello World!";
+        message = "Hello Pepe!";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
 
+
         // Hello
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
+        out.println(db.getConnection());
         out.println("<h1>" + message + "</h1>");
         out.println("</body></html>");
     }
