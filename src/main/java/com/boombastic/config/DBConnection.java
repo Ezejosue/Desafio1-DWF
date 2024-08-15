d0685665a76892ba1c1423b34b03bd8b3ea249c8
razmit <rolin.azmitia.iii@gmail.com>
2024-08-15 00:04:25 -0600

    Progressed in the Dept CRUD

--- src/main/java/com/boombastic/config/DBConnection.java
+++ src/main/java/com/boombastic/config/DBConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DBConnection {
     
-    public Connection getConnection() {
+    public static Connection getConnection() {
         Connection con;
 
         try {
--- src/main/java/com/boombastic/controller/DepartmentController.java
+++ src/main/java/com/boombastic/controller/DepartmentController.java
@@ -1,9 +1,50 @@
 package com.boombastic.controller;
 
+import com.boombastic.config.DBConnection;
+import com.boombastic.model.Department;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet("/departments")
 public class DepartmentController extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        List<Department> departments = new ArrayList<>();
+        try {
+            Connection con = DBConnection.getConnection();
+            String sql = "select * from departamento";
+
+            PreparedStatement stmt = con.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                Department department = new Department();
+                department.setId(rs.getInt("idDepartamento"));
+                department.setDept_name(rs.getString("nombreDepartamento"));
+                department.setDept_description(rs.getString("descripcionDepartamento"));
+                departments.add(department);
+            }
+
+            rs.close();
+            stmt.close();
+            con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        request.setAttribute("departments", departments);
+        request.getRequestDispatcher("/WEB-INF/jsp/departments.jsp").forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        doGet(request, response);
+    }
 }
